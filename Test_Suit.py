b36b49402101d727de4f2ce414b7ba411b868304
lazarica-petrut-work <lazarica.petrut.work@gmail.com>
2023-08-10 08:06:01 +0300

    Added Templates

--- Test_Suit.py
+++ Test_Suit.py
@@ -1,1 +1,27 @@
-# Hello
+import unittest
+
+from HTMLTestRunner.runner import HTMLTestRunner
+from Unit_Test import TestCase
+
+
+# Test Suit Class
+class TestingClass(unittest.TestCase):
+
+    # Test Suit for all the other tests
+    def test_suit(self):
+        my_test_suit = unittest.TestSuite()
+        my_test_suit.addTests([
+            unittest.defaultTestLoader.loadTestsFromTestCase(TestCase),
+            #unittest.defaultTestLoader.loadTestsFromTestCase(TestCase2)#,
+            #unittest.defaultTestLoader.loadTestsFromTestCase(EasyTest)
+        ])
+
+        # Test Runner HTML Report
+        my_test_runnner = HTMLTestRunner(
+            output = "report",
+            title = "Test report",
+            report_name = "report",
+            tested_by = "Lazarica Petrut"
+        )
+
+        my_test_runnner.run(my_test_suit)
